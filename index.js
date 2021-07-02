52a1c4221c0eb1cd9a80da6f9f12591be8892635
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-07-02 17:20:06 +0800

    strange

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ $(function () {
 });
 function init() {
     document.getElementById("style_width_280px").style.width = "25vw";
-    document.getElementById("style_RIGHT").style.width = "74vw";
     document.getElementById("width_1_5rem").style.width = "1vw";
     
 }
