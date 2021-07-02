40ff0a306bbe0e40f1df3a101f625883f308c412
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-07-02 21:44:01 +0800

    ok

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ function hexc(colorval) {
     color = '#' + parts.join('');
     return color;
 }
+function date_time_now() {
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var d = new Date();
+    var day = days[d.getDay()];
+    var hr = d.getHours();
+    var min = d.getMinutes();
+    if (min < 10) {
+        min = "0" + min;
+    }
+    var ampm = "am";
+    if (hr > 12) {
+        hr -= 12;
+        ampm = "pm";
+    }
+    var date = d.getDate();
+    var month = months[d.getMonth()];
+    var year = d.getFullYear();
+    return (day + "_" + hr + "_" + min + ampm + "_" + date + "_" + month + "_" + year);
+}
 function init(callback) {
     document.getElementById("style_width_280px").style.width = "25vw";
     document.getElementById("width_1_5rem").style.width = "1vw";
@@ -37,7 +57,7 @@ function after_init() {
         var obj_all_data_to_save = [];
         document.getElementById("ascii_row_pattern_hex").innerText = "";
         document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText = "{";
-        document.getElementById("ascii_row_pattern_hex_with_data_structure_define").innerText = "";
+        //document.getElementById("ascii_row_pattern_hex_with_data_structure_define").innerText = "";
         for (let index = 0; index < document.getElementById("what_to_snap_body").getElementsByClassName("row_class").length; index++) {
             const element = document.getElementById("what_to_snap_body").getElementsByClassName("row_class")[index];
             var int_row_hex = 0;
@@ -49,9 +69,37 @@ function after_init() {
             }
             obj_all_data_to_save.push(obj_all_data_to_save_sub);
             document.getElementById("ascii_row_pattern_hex").innerText += int_row_hex.toString(16);
-            document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText += ("0x" + ((int_row_hex < 10) ? "0" : "") + int_row_hex.toString(16).toUpperCase() + `, `);
+            document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText += ("0x" + ((int_row_hex < 10) ? "0" : "") + int_row_hex.toString(16).toUpperCase() + ",");
         }
-        document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText = document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText.slice(0, -1).slice(0, -1) + "}";
-        document.getElementById("get_infoV").innerText = JSON.stringify(obj_all_data_to_save);
+        document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText = document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText.slice(0, -1) + "}";
+        document.getElementById("obj_all_data_to_save").innerText = JSON.stringify(obj_all_data_to_save);
+        document.getElementById("bton_data_fake").innerText = "1";
+    });
+    document.getElementById('btn_index_text').addEventListener('click', () => {
+        document.getElementById("bton_data_fake").innerText = "-1";
+        document.getElementById("bton_data_fake").click();
+        function checkFlag() {
+            if (document.getElementById('bton_data_fake').innerText !== '1') {
+                setTimeout(() => {
+                    checkFlag();
+                }, 5);
+            } else {
+                $('#val_index_text').val((!$('#val_index_text').val()) ? date_time_now() : $('#val_index_text').val());
+                document.getElementById("all_commits").innerHTML = document.getElementById("all_commits").innerHTML + `<tr class="index_count">
+<th scope="col">${(!document.getElementsByClassName("index_count")) ? "0" : String(document.getElementsByClassName("index_count").length)}</th>
+<th scope="col">${$('#val_index_text').val()}</th>
+<th scope="col">
+<button type="button" class="btn btn-primary trigger_version_change" id="${(!document.getElementsByClassName("index_count")) ? "0" : String(document.getElementsByClassName("index_count").length)}_trigger_edit">edit</button>
+</th><th scope="col">
+<button type="button" class="btn btn-warning trigger_version_change" id="${(!document.getElementsByClassName("index_count")) ? "0" : String(document.getElementsByClassName("index_count").length)}_trigger_del">del</button>
+</th>
+<th scope="col">${document.getElementsByClassName("ascii_row_pattern_hex").innerText}</th>
+<th scope="col">${document.getElementsByClassName("ascii_row_pattern_hex_with_data_structure").innerText}</th>
+<th scope="col">#define ASCII88PATTERN_${$('#val_index_text').val()} ${document.getElementsByClassName("ascii_row_pattern_hex_with_data_structure").innerText}</th>
+<th scope="col">${document.getElementsByClassName("obj_all_data_to_save").innerText}</th>
+</tr>`;
+            }
+        }
+        checkFlag();
     });
 }
