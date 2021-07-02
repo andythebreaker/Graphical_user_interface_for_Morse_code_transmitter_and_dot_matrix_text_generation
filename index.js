d76f64d9d5744700fedf12b6b0a743279cec9f3e
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-07-02 20:53:32 +0800

    ,,

--- index.js
+++ index.js
@@ -34,24 +34,24 @@ function after_init() {
         $(this).css("background-color", (hexc(String($(this).css("background-color"))) === "#ddb98b") ? "blue" : "#ddb98b");
     });
     $('.bton_data').click(function (event) {
-        var obj_all_data_to_save=[];
-        document.getElementById("ascii_row_pattern_hex").innerText="";
-        document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText="{";
-        document.getElementById("ascii_row_pattern_hex_with_data_structure_define").innerText="";
+        var obj_all_data_to_save = [];
+        document.getElementById("ascii_row_pattern_hex").innerText = "";
+        document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText = "{";
+        document.getElementById("ascii_row_pattern_hex_with_data_structure_define").innerText = "";
         for (let index = 0; index < document.getElementById("what_to_snap_body").getElementsByClassName("row_class").length; index++) {
             const element = document.getElementById("what_to_snap_body").getElementsByClassName("row_class")[index];
-            var int_row_hex=0;
-            var obj_all_data_to_save_sub=[];
+            var int_row_hex = 0;
+            var obj_all_data_to_save_sub = [];
             for (let index2 = 0; index2 < element.getElementsByClassName("square").length; index2++) {
                 element2_id = "cell_numb_" + String(index) + String(index2);
-                int_row_hex+=(hexc(String($(`#${element2_id}`).css("background-color"))) === "#ddb98b") ? 0:Math.pow(2,index2);
-                obj_all_data_to_save_sub.push((hexc(String($(`#${element2_id}`).css("background-color"))) === "#ddb98b") ? false:true);
+                int_row_hex += (hexc(String($(`#${element2_id}`).css("background-color"))) === "#ddb98b") ? 0 : Math.pow(2, index2);
+                obj_all_data_to_save_sub.push((hexc(String($(`#${element2_id}`).css("background-color"))) === "#ddb98b") ? false : true);
             }
             obj_all_data_to_save.push(obj_all_data_to_save_sub);
-            document.getElementById("ascii_row_pattern_hex").innerText+=int_row_hex.toString(16);
-            document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText+=("0x"+int_row_hex.toString(16)+",");
+            document.getElementById("ascii_row_pattern_hex").innerText += int_row_hex.toString(16);
+            document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText += ("0x" + ((int_row_hex < 10) ? "0" : "") + int_row_hex.toString(16).toUpperCase() + `, `);
         }
-        document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText=document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText.slice(0, -1).slice(0, -1)+"}";
-        document.getElementById("ascii_row_pattern_hex_with_data_structure_define").innerText=JSON.stringify(obj_all_data_to_save);
+        document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText = document.getElementById("ascii_row_pattern_hex_with_data_structure").innerText.slice(0, -1).slice(0, -1) + "}";
+        document.getElementById("get_infoV").innerText = JSON.stringify(obj_all_data_to_save);
     });
 }
