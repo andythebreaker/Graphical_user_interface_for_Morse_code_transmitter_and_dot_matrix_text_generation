1c57fa3dc9edd45546fd64334df2bc9b9aad4157
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-07-02 18:02:07 +0800

    click

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 
 $(function () {
-
-    init();
+    init(after_init());
 });
-function init() {
+function init(callback) {
     document.getElementById("style_width_280px").style.width = "25vw";
     document.getElementById("width_1_5rem").style.width = "1vw";
-    
+    for (let index = 0; index < document.getElementById("what_to_snap_body").getElementsByClassName("row_class").length; index++) {
+        const element = document.getElementById("what_to_snap_body").getElementsByClassName("row_class")[index];
+        for (let index2 = 0; index2 < element.getElementsByClassName("square").length; index2++) {
+            const element2 = element.getElementsByClassName("square")[index2];
+            element2.id = String(element.id) + String(index2);
+            //element2.innerText = String(index) + String(index2);
+        }
+    }
+    // 如果 callback 是個函式就呼叫它
+    if (typeof callback === 'function') {
+        callback();
+    }
+}
+function after_init() {
+    $('.square :after').click(function () {
+        $(this).css("background-color", "blue");
+    });
 }
